68147ff1c6f184cb29f75827d3552889903e5789
mherrera31 <maherrera31@gmail.com>
2025-08-14 21:16:56 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Rellena esto con tu proyecto de Supabase.
-// Sugerencia: si tu URL de funciones es https://abc123.functions.supabase.co
-// entonces tu URL del proyecto es https://abc123.supabase.co
-export const SUPABASE_URL = "https://ztdkaxbgpogudtrdgzic.supabase.co"; // <- cambia si no coincide
-export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po"; // pega tu anon key pública
