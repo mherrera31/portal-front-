0c8cf8ada6421656585aae8562ff5831a1ec1103
mherrera31 <maherrera31@gmail.com>
2025-08-14 21:26:25 -0500

    Delete adminpsc directory

--- adminpsc/config.js
+++
@@ -1,5 +0,0 @@
-// config.js - PSC Portal
-window.__PSC_CONFIG__ = {
-  SUPABASE_URL: "https://ztdkaxbgpogudtrdgzic.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po"
-};
