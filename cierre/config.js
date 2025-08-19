655c50d64f06a2f5c1493fff987bf160d6e8fed0
mherrera31 <maherrera31@gmail.com>
2025-08-19 18:49:05 -0500

    Update config.js

--- cierre/config.js
+++ cierre/config.js
@@ -1,14 +1,7 @@
-// config.js
-window.PSC_CONFIG = {
-  // opcional si pones url:
-  ref: "ztdkaxbgpogudtrdgzic",
-  // URL del proyecto (si pones ref, la URL puede omitirse)
-  url: "https://ztdkaxbgpogudtrdgzic.supabase.co",
-  // Anon Key de Supabase
-  anon: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po",
-  // Bucket de storage para vouchers/fotos
-  bucket: "cierre-comprobantes",
-  // Logo (puedes cambiarlo)
-  logo: "https://i.postimg.cc/L8w6XpRT/PSC-AVION.webp"
-};
-
+<script>
+  window.PSC_CONFIG = {
+    supabaseUrl: "https://ztdkaxbgpogudtrdgzic.supabase.co",
+    supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po",
+    functionsBase: "https://ztdkaxbgpogudtrdgzic.functions.supabase.co"
+  };
+</script>
