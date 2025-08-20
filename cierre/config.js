9c51da73a2f65f718699fe14ed8ec8cd8fb1a6cd
mherrera31 <maherrera31@gmail.com>
2025-08-19 19:07:11 -0500

    Update config.js

--- cierre/config.js
+++ cierre/config.js
@@ -1,7 +1,6 @@
-<script>
+
   window.PSC_CONFIG = {
     supabaseUrl: "https://ztdkaxbgpogudtrdgzic.supabase.co",
     supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po",
     functionsBase: "https://ztdkaxbgpogudtrdgzic.functions.supabase.co"
   };
-</script>
