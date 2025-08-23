68ccc61858c0a7ed82837b8bcc8f158b9c26981b
mherrera31 <maherrera31@gmail.com>
2025-08-23 13:24:07 -0500

    Update config.js

--- adminpsc/config.js
+++ adminpsc/config.js
@@ -6,3 +6,4 @@ window.__PSC_CONFIG__ = {
   apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70PoY",
   functionsBase: "https://ztdkaxbgpogudtrdgzic.functions.supabase.co", // opcional (solo para export)
   userEmail: "tito@psc.com" // <- SIN LOGIN: el usuario sale de aquí
+};
