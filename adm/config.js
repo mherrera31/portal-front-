591df239a03a70d95d8b387f085f45d140720037
mherrera31 <maherrera31@gmail.com>
2025-08-23 13:28:23 -0500

    Create config.js

---
+++ adm/config.js
@@ -0,0 +1,6 @@
+window.PSC_CONFIG = {
+  url: "https://tu-proyecto.supabase.co",
+  apikey: "TU-ANON-KEY",
+  functionsBase: "https://tu-proyecto.functions.supabase.co", // opcional (solo export)
+  userEmail: "operaciones@psc.com"                             // si vas “sin login”
+};
