f35f92a1ec7f2981b1fa9548877beeeee6b45788
mherrera31 <maherrera31@gmail.com>
2025-08-23 13:30:00 -0500

    Update config.js

--- adm/config.js
+++ adm/config.js
@@ -1,6 +1,6 @@
 window.PSC_CONFIG = {
-  url: "https://tu-proyecto.supabase.co",
-  apikey: "TU-ANON-KEY",
-  functionsBase: "https://tu-proyecto.functions.supabase.co", // opcional (solo export)
+  url: "https://ztdkaxbgpogudtrdgzi.supabase.co",
+  apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po",
+  functionsBase: "https://ztdkaxbgpogudtrdgzi.functions.supabase.co", // opcional (solo export)
   userEmail: "operaciones@psc.com"                             // si vas “sin login”
 };
