2c37f76c69519bbf00ac00de1ed145440828cb5d
mherrera31 <maherrera31@gmail.com>
2025-10-04 20:07:21 -0500

    Update config.js

--- ventas/config.js
+++ ventas/config.js
@@ -1,1 +1,7 @@
+// Archivo: config.js
 
+// --- CONFIGURACIÓN DE CONEXIÓN A SUPABASE ---
+// En este archivo guardamos las llaves para mantener el código principal limpio.
+
+const SUPABASE_URL = 'https://ztdkaxbgpogudtrdgzic.supabase.co'; 
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0ZGtheGJncG9ndWR0cmRnemljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4NDE2MDYsImV4cCI6MjA3MDQxNzYwNn0.mpjY5nWiS48O0DAUkCL7Jl6agF6LurNGeaY8d1u70Po';
